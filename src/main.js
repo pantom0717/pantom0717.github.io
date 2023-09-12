04e6fdb550166527134efbbd7f715b5ccb428949
pantom0717 <90772215+pantom0717@users.noreply.github.com>
2023-09-13 02:35:06 +0900

    js update

---
+++ src/main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const header = document.querySelector('.header');
+const headerHeight = header.offsetHeight;
+document.addEventListener('scroll', () => {
+  if (window.scrollY > headerHeight) {
+    header.classList.add('header--dark');
+  } else {
+    header.classList.remove('header--dark');
+  }
+});
+
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+
+const navbarMenu = document.querySelector('.header__menu');
+const navbarToggle = document.querySelector('.header__toggle');
+navbarToggle.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+});
+
+
+navbarMenu.addEventListener('click', () => {
+  navbarMenu.classList.remove('open');
+});
